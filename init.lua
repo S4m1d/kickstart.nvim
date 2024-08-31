4406e75afa64487c2f3c17ed01a3ab1aaf81c98e
s4m1d <iams4m1d@gmail.com>
2024-08-31 15:42:14 +0500

    added jq autoinstall

--- init.lua
+++ init.lua
@@ -594,6 +594,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'jq', -- formatter for json
         -- no need to use clang-format because there is builtin formatting in clangd
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
