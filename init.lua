ed8eaa6fb313db8dc8357d31dbcfd8a2a7625c50
s4m1d <iams4m1d@gmail.com>
2024-08-31 18:36:45 +0500

    switched rust config from bare rust-analyzer to rustaceanvim plugin

--- init.lua
+++ init.lua
@@ -555,7 +555,8 @@ require('lazy').setup({
         gopls = {},
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
+        -- rust lsp is now configured in rustaceanvim plugin
+        --rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
