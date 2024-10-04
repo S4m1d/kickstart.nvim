49057032e6d67fb5242f939670740e63fe0a514f
s4m1d <iams4m1d@gmail.com>
2024-10-05 01:47:01 +0500

    made theme transparent

--- init.lua
+++ init.lua
@@ -759,15 +759,18 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
+    config = function(_, opts)
+      require('tokyonight').setup(opts) -- Pass options to the setup function
+      vim.cmd 'colorscheme tokyonight' -- Set the colorscheme
     end,
   },
 
